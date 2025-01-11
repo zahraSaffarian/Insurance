d65f7296ee65a6d55dd48dda6bdda35184b96286
z.saffarian <z.saffarian@snappgrocery.com>
2025-01-11 09:39:43 +0330

    rtl

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -26,9 +26,10 @@ const Input: FC<InputProps> = ({
         value={value}
         onChange={onChange ? (e) => onChange(e.target.value) : undefined}
         placeholder={placeholder}
+        dir="rtl"
         {...register}
         {...rest}
-        className={`block w-full px-3 py-2 border transition-all duration-300 ease-in-out ${
+        className={`block w-full px-3 py-2 border transition-all duration-300 ease-in-out  ${
           error ? "border-red-500" : "border-gray-300"
         } rounded-md shadow-sm focus:outline-none focus:ring focus:ring-indigo-200`}
       />
